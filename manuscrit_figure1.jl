f6b012e892ccd9c133c45c48ad6a2ed6099f813f
sauves <leonardsauve@gmail.com>
2024-02-13 16:26:26 -0500

    included Clinical Features to Lgn

--- manuscrit_figure1.jl
+++ manuscrit_figure1.jl
@@ -22,6 +22,33 @@ outpath, session_id = set_dirs() ;
 tcga_datasets_list = ["Data/TCGA_datasets/$(x)" for x in readdir("Data/TCGA_test") ]
 BRCA_data = load_tcga_datasets(tcga_datasets_list)["BRCA"]
 DataDict = Dict("name"=>"LgnAML","dataset" => MLSurvDataset("Data/LEUCEGENE/LGN_AML_tpm_n300_btypes_labels_surv.h5"))
+lgn_CF = CSV.read("Data/LEUCEGENE/lgn_pronostic_CF", DataFrame)
+function numerise_labels(DF, colnames)
+    MMs = []
+    level_names = []
+    for colname in colnames
+        levels = unique(DF[:, colname])
+        if length(levels) > 2
+            levels_dict = Dict([(lab, idx) for (idx, lab) in enumerate(levels)])
+            MM = zeros(size(DF)[1], size(levels)[1])
+            for level in levels
+                MM[DF[:,colname].==level, levels_dict[level]] .= 1
+            end
+            push!(level_names, ["$(colname)_$(level)" for level in levels])
+        else 
+            MM = zeros(size(DF)[1],1)
+            MM[DF[:,colname] .== levels[2]] .= 1
+            push!(level_names, "$(colname)_$(levels[2])")
+        end 
+
+        push!(MMs, MM)
+        
+    end 
+    return hcat(MMs...), vcat(level_names...) 
+end
+CF_bin, lnames = numerise_labels(lgn_CF, ["Sex","Cytogenetic risk", "NPM1 mutation", "IDH1-R132 mutation", "FLT3-ITD mutation", ])
+push!(lnames, "Age")
+clinical_factors = hcat(CF_bin, lgn_CF[:,"Age_at_diagnosis"])
 # DataDict = BRCA_data
 
 # dataset_name = "BRCA"
@@ -29,17 +56,24 @@ dataset_name = "LgnAML"
 dim_redux_type = "RDM"
 DATA = DataDict["dataset"] 
 
-keep = [occursin("protein_coding", bt) for bt in DATA.biotypes]
-ngenes = sum(keep)
-println("$dataset_name nb genes : $(ngenes)")
+# keep = [occursin("protein_coding", bt) for bt in DATA.biotypes]
+# ngenes = sum(keep)
+println("$dataset_name nb genes : $(size(DATA.genes)[1])")
 println("$dataset_name nb patients : $(size(DATA.samples)[1])")
 println("$dataset_name % uncensored : $(round(mean(DATA.surve .!= 0), digits=3) * 100)%")
 dim_redux_list = [0,1,2,3,4,5,10,15,20,25,50,75,100,125,250,375,500,625,ngenes]
 hlsize = 512
-cph_nb_hl, modeltype, nepochs= 2,  "cphdnn", 10000
+cph_nb_hl, modeltype, nepochs= 2,  "cphdnn", 5000
 # for dim size in list do 
-dim_redux_size = 15#dim_redux_list[end]
-nb_clinf = 0
+# select and process using dim. redux
+LSC17 = CSV.read("Data/SIGNATURES/LSC17.csv", DataFrame)
+keep = [x in LSC17[:,"alt_name"] for x in DATA.genes ]
+keep = [occursin("protein_coding",x) for x in DATA.biotypes]
+dredux_data = Matrix(DATA.data[:,keep])
+dim_redux_size =0# size(dredux_data)[2]
+nb_clinf = size(clinical_factors)[2]
+final_data = hcat(dredux_data, clinical_factors)
+final_data = clinical_factors
 
 base_params = Dict(
         ## run infos 
@@ -47,7 +81,7 @@ base_params = Dict(
         "machine_id"=>strip(read(`hostname`, String)), "device" => "$(device())",
         "dim_redux_type"=>dim_redux_type, "dataset" => dataset_name,
         ## optim infos 
-        "nepochs" => nepochs, "ae_lr" =>0, "cph_lr" => 1e-3, "ae_wd" => 0, "cph_wd" => 0.00001,
+        "nepochs" => nepochs, "ae_lr" =>0, "cph_lr" => 1e-6, "ae_wd" => 0, "cph_wd" => 0.1,
         ## model infos
         "model_type"=> modeltype, "ae_nb_hls" => 2,
         "enc_nb_hl" => 0, "enc_hl_size"=> 0,
@@ -55,10 +89,6 @@ base_params = Dict(
         "cph_nb_hl" => cph_nb_hl, "cph_hl_size" => hlsize
         ## metrics
 )
-# select and process using dim. redux
-LSC17 = CSV.read("Data/SIGNATURES/LSC17.csv", DataFrame)
-keep = [x in LSC17[:,"alt_name"] for x in DATA.genes ]
-dredux_data = Matrix(DATA.data[:,keep])
 
 # init params dict
 DataDict["params"] = deepcopy(base_params)
@@ -72,7 +102,7 @@ DataDict["params"]["nsamples_train"] = size(DATA.samples)[1] - Int(round(size(DA
 DataDict["params"]["model_title"] = "$(dataset_name)_$(modeltype)_$(dim_redux_type)_$(nb_clinf)CF"
 DataDict["params"]["insize"] = DataDict["params"]["dim_redux"] + DataDict["params"]["nb_clinf"]
 # split folds 
-folds = split_train_test(dredux_data, DATA.survt, DATA.surve, DATA.samples;nfolds =DataDict["params"]["nfolds"])
+folds = split_train_test(final_data, DATA.survt, DATA.surve, DATA.samples;nfolds =DataDict["params"]["nfolds"])
 OUTS_TST = []
 Y_T_TST = []
 Y_E_TST = []
@@ -86,10 +116,10 @@ for fold in folds
     ## init model 
     cph_opt = Flux.ADAM(base_params["cph_lr"]) 
     cph_wd = base_params["cph_wd"]
-    # cphdnn = gpu(Chain(Dense(DataDict["params"]["insize"],base_params["cph_hl_size"], leakyrelu), 
-    # Dense(base_params["cph_hl_size"],base_params["cph_hl_size"], leakyrelu), 
-    # Dense(base_params["cph_hl_size"], 1, bias = false)))
-    cphdnn = gpu(Chain(Dense(DataDict["params"]["insize"], 1, bias = false)))
+    cphdnn = gpu(Chain(Dense(DataDict["params"]["insize"],base_params["cph_hl_size"], leakyrelu), 
+    Dense(base_params["cph_hl_size"],base_params["cph_hl_size"], leakyrelu), 
+    Dense(base_params["cph_hl_size"], 1, bias = false)))
+    # cphdnn = gpu(Chain(Dense(DataDict["params"]["insize"], 1, bias = false)))
     for i in 1:DataDict["params"]["nepochs"]
         cph_ps = Flux.params(cphdnn)
         cph_gs = gradient(cph_ps) do 
@@ -129,3 +159,4 @@ up_ci = round(sorted_cinds[Int(floor(bootstrap_n * 0.975 ))], digits = 3)
 med_c_ind = round(median(sorted_cinds), digits = 3)
 println("TEST bootstrap c-index : $(med_c_ind) ($up_ci - $lo_ci 95% CI)")
 # merge results, obtain risk scores, test data.
+OUTS_TST[2]
